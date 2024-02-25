3d9962ed5264a6b96772f0036f288cf697968668
Hellen Busuru <hellenbusuru@gmail.com>
2024-02-26 02:15:22 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where are you located? ")
+
+# Print out personalized message
+print("Hello {}, you are {} years old and you live in {}.".format(name, age, location))
